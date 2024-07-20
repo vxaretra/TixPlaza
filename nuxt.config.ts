77fcd4c09f78e7a53c19c276002175550cee53a0
pandaz <rahadifauzan7@gmail.com>
2024-07-20 12:55:12 +0700

    setup fe

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  plugins: [ 
+  ],
+
+  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+        theme: {
+            preset: Aura
+        }
+    }
+  }
 })
---
+++ pages/index/auth/register/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="register-form">
+    <div class="h-52 w-1/2 mx-auto mt-5">
+      <Splitter>
+        <SplitterPanel class="" :size="65">
+          <h2 class="text-5xl">Register</h2>
+          <div class="w-1/2 mx-auto space-y-10">
+            <FloatLabel>
+              <IconField>
+                <InputIcon class="pi pi-search" />
+                <InputText
+                  v-model="value"
+                  inputId="ac"
+                  :suggestions="items"
+                  @complete="search"
+                  :style="{ width: '400px' }"
+                />
+                <label for="ac">Float Label</label>
+              </IconField>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText
+                v-model="value"
+                inputId="ac"
+                :suggestions="items"
+                @complete="search"
+                :style="{ width: '400px' }"
+              />
+              <label for="ac">Float Label</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText
+                v-model="value"
+                inputId="ac"
+                :suggestions="items"
+                @complete="search"
+                :style="{ width: '400px' }"
+              />
+              <label for="ac">Float Label</label>
+            </FloatLabel>
+            <FloatLabel>
+              <InputText
+                v-model="value"
+                inputId="ac"
+                :suggestions="items"
+                @complete="search"
+                :style="{ width: '400px' }"
+              />
+              <label for="ac">Float Label</label>
+            </FloatLabel>
+          </div>
+        </SplitterPanel>
+        <SplitterPanel class="flex items-center justify-center" :size="35">
+          <Image src="/img/ticket.jpg" alt="Image" />
+        </SplitterPanel>
+      </Splitter>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      items: [],
+    };
+  },
+  methods: {
+    search(event) {
+      this.items = [...Array(10).keys()].map(
+        (item) => event.query + "-" + item
+      );
+    },
+  },
+};
+</script>
+
+<style scoped></style>
