5b6823341d56b24de41e83dd81c1ecf16b516191
vxaretra <andrewrizqi00@gmail.com>
2024-07-21 14:16:02 +1000

    refactor registration

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -2,10 +2,8 @@ import vine, { errors } from "@vinejs/vine"
 import { ReqPostRegister } from "~/dto/auth"
 import { prisma } from "~/prisma/db"
 
-export default defineEventHandler(async (event) => {
+async function validatePostRegister(req: ReqPostRegister) {
   try {
-    const body = await readBody<ReqPostRegister>(event);
-
     const schema = vine.object({
       email: vine.string().trim().email(),
       password: vine.string().trim().minLength(6).maxLength(32),
@@ -13,32 +11,33 @@ export default defineEventHandler(async (event) => {
       phoneNumber: vine.string().trim().mobile(),
     });
 
-    await vine.validate({ schema: schema, data: body });
-
-    const existingUser = await prisma.user.findFirst({ where: { email: body.email } });
-    if (existingUser !== null) {
-      setResponseStatus(event, 400);
-      return { error: "Email already exists" };
-    }
-
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-        name: body.name,
-        phoneNumber: body.phoneNumber,
-      }
-    })
-
-    setResponseStatus(event, 201);
-    return { email: user.email };
+    await vine.validate({ schema: schema, data: req });
   } catch (error) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
-      setResponseStatus(event, 400);
-      return { error: error.messages };
+      throw createError({ statusCode: 400, statusMessage: "Bad Request", message: "Invalid input", data: error.messages });
     }
-
-    setResponseStatus(event, 500);
-    return { error: "Internal Server Error" };
   }
+}
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody<ReqPostRegister>(event);
+
+  await validatePostRegister(body);
+
+  const existingUser = await prisma.user.findFirst({ where: { email: body.email } });
+  if (existingUser !== null) {
+    throw createError({ statusCode: 400, statusMessage: "Bad Request", message: "Email already exists" });
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+      name: body.name,
+      phoneNumber: body.phoneNumber,
+    }
+  })
+
+  setResponseStatus(event, 201);
+  return { email: user.email };
 })
