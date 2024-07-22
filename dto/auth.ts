35c17a8fa7927af59ecad83e377cfe9bce3e3f97
vxaretra <andrewrizqi00@gmail.com>
2024-07-22 14:37:03 +1000

    add login api handler

--- dto/auth.ts
+++ dto/auth.ts
@@ -11,6 +11,18 @@ export type ResPostRegister = {
     data: { token: string }
 }
 
+export type ReqPostLogin = {
+    email: string
+    password: string
+}
+
+export type ResPostLogin = {
+    data: {
+        token: string
+        isVerified: boolean
+    }
+}
+
 export type AuthTokenPayload = {
     id: number
     name: string
