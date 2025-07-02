ef8d86136aa72e2a97a512cda3e7ab37cadce719
vxaretra <andrewrizqi00@gmail.com>
2025-07-02 20:50:15 +1000

    setup mail

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     smtpHost: "",
+    smtpPort: "",
     smtpUser: "",
     smtpPass: "",
     xenditKey: "",
--- server/utils/mailer.ts
+++ server/utils/mailer.ts
@@ -11,19 +11,10 @@ const transport = nodemailer.createTransport({
 });
 
 export async function emailVerificationCode(to: string, code: number) {
-    const body = {
-        "from": "TixPlaza <tixplaza@resend.dev>",
-        "to": to,
-        "subject": "Account Verification",
-        "text": `Your verification code: ${code}`,
-    };
-
     const info = await transport.sendMail({
-        from: '"TixPlaza <tixplaza@mailtrap.dev>', // sender address
+        from: "TixPlaza <tixplaza@mailtrap.dev>", // sender address
         to: to, // list of receivers
         subject: "Account Verification", // Subject line
         text: `Your verification code is: ${code}`, // plain text body
     });
-
-    console.info(info);
 }
